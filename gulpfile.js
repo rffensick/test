6fd896ea16ac4757fd868df3108eb6ca93856b87
Edward <ilyaskin1998@gmail.com>
2017-08-08 19:30:07 +0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -103,9 +103,9 @@ gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
 gulp.task('deploy', function() {
 
 	var conn = ftp.create({
-		host:      'rffensick.ru',
-		user:      'ilyaskkq_rffensick',
-		password:  '1998edya',
+		host:      '',
+		user:      '',
+		password:  '',
 		parallel:  10,
 		log: gutil.log
 	});
